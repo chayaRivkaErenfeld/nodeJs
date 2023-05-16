30b454a44c4004c4ade5b4f4dbb1e4cdcf8b9f02
chayaRivkaErenfeld <erenfeld226@gmail.com>
2023-05-16 15:17:55 +0300

    add cors

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 const express=require('express')
-
 const app=express()
+
+//cors in middleware
+//מיד לכל req הוא מוסיף ל res  את ההרשאות
+app.use((req, res, next)=>{
+    //מאפשר בקשות מכל מקור
+    res.header('Access-Control-Allow-Origin', '*')
+    //רשימה של כותרות אפשריות
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    //other method
+    if (req.method === 'OPTIONS') {
+                res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+                res.status(200).send()
+     }
+     next()
+})
+
 
 app.get('/',(req, res)=>{
     res.status(200).json({
         messege:"hello world 0"
     })
+}) 
+
+//לנתיב לא קיים
+app.use((req, res, next) => {
+    const error = new Error("NOT FOUND :(")
+    error.status = 404
+    next(error)
 })
 
+app.use((error, req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error:{messege:error.messege}
+    })
+})
 module.exports = app
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http =require('http')
 const app = require('./app')
 const port = 3000
-//hellow rina rina 
+
 const server = http.createServer(app)
+
 server.listen(port)
 
